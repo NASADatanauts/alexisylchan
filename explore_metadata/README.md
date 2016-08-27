a091231fe80bc3bdc8254d84c870ac02846dd824
Alexis Chan <alexis.cyl@gmail.com>
2016-08-27 08:34:56 -0400

    Publish LDA work

---
+++ explore_metadata/README.md
@@ -0,0 +1,2 @@
+# nasa_datanauts
+Datanauts exploration
--- explore_metadata/parse_metadata.py
+++ explore_metadata/parse_metadata.py
@@ -1,26 +1,41 @@
-# To use this script you have to install ijson. See https://pypi.python.org/pypi/ijson/
-# Usage: python parse_metadata.py
-import ijson
+def write_property(data_objects, prop_name):
+    import json
+    count = 0
+    summary = []
+     # grab only id and specified property 
+    for obj in data_objects:
+        for dataset in obj:
+            summary.append({ \
+                "id": dataset["_id"]["$oid"], \
+                 prop_name:dataset.get(prop_name, '') \
+                 })          
+        count = count + 1
+        if count > 5:
+            break
 
-# nasa_metadata2.json is the file downloaded from https://data.nasa.gov/data.json
-filename='nasa_metadata2.json'
-f = open(filename, 'rb')
-
-# nasa_metadata2.json is the file downloaded from https://data.nasa.gov/data.json
-objects = ijson.items(f, 'dataset');
-count = 0
-summary = []
-
-# grab only title and description 
-for obj in objects:
-    for dataset in obj:
-        summary.append({"title":dataset["title"
-            ],"description":dataset["description"]});
-    count = count + 1
-    if count > 5:
-        break
-
-# write title and description to new json file
-import json
-with open('nasa_text2.json', 'w') as outfile:
+    # write title and description to new json file
+    outfile = open('nasa_' + prop_name + '.json', 'w')
     json.dump(summary, outfile)
+
+
+def parse_metadata(prop_name):
+    # To use this script you have to install ijson. See https://pypi.python.org/pypi/ijson/
+    # Usage: python parse_metadata.py
+    import ijson
+
+    # nasa_metadata2.json is the file downloaded from https://data.nasa.gov/data.json
+    filename ='nasa_metadata2.json'
+    infile = open(filename, 'rb')
+
+    data_objects = ijson.items(infile, 'dataset')
+    write_property(data_objects, prop_name)
+   
+parse_metadata('title')
+parse_metadata('description')
+parse_metadata('keyword')
+parse_metadata('landingPage')
+parse_metadata('language')
+parse_metadata('theme')
+parse_metadata('temporal')
+parse_metadata('spatial')
+parse_metadata('modified')
